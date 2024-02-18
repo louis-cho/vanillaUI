f6e562c85e0131174e26c48ce764824fbefa2e8e
ChoHyunWoo <wizardchw0@gmail.com>
2024-02-18 21:24:23 +0900

    [FE Feat] 사이드 바 추가 + 스타일링

---
+++ vanillaPreference.js
@@ -0,0 +1,6 @@
+let preference = {};
+
+preference.color = {};
+preference.color.primary = "#00ff00";
+
+export default { preference };
--- vanillaSideBar.js
+++ vanillaSideBar.js
@@ -1,78 +1,52 @@
 export class VanillaSideBar {
-    constructor(div, name) {
-      this._div = div;
-  
-      this._name = name;
-  
-      VanillaSideBar.I = this;
-  
-      this.init();
-    }
-  
-    init() {
-      let innerHTML = this._createHTML();
-      this._appendHTML(innerHTML);
-    }
-  
-    _appendHTML(innerHTML) {
-      this._div.innerHTML = innerHTML;
-    }
-  
-    _createHTML() {
-      let name = this._name;
-      let ihtml = [];
-      let idx = 0;
-  
-      ihtml[idx++] = "<aside class=\"side-bar\">";
-      ihtml[idx++] = "  <section class=\"side-bar__icon-box\">";
-      ihtml[idx++] = "    <section class=\"side-bar__icon-1\">";
-      ihtml[idx++] = "      <div></div>";
-      ihtml[idx++] = "      <div></div>";
-      ihtml[idx++] = "      <div></div>";
-      ihtml[idx++] = "    </section>";
-      ihtml[idx++] = "  </section>";
-      ihtml[idx++] = "  <ul>";
-      ihtml[idx++] = "    <li>";
-      ihtml[idx++] = "      <a href=\"#\"><i class=\"fa-solid fa-cat\"></i> menu1</a>";
-      ihtml[idx++] = "      <ul>";
-      ihtml[idx++] = "        <li><a href=\"#\">text1</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text2</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text3</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text4</a></li>";
-      ihtml[idx++] = "      </ul>";
-      ihtml[idx++] = "    </li>";
-      ihtml[idx++] = "    <li>";
-      ihtml[idx++] = "      <a href=\"#\">menu2</a>";
-      ihtml[idx++] = "      <ul>";
-      ihtml[idx++] = "        <li><a href=\"#\">text1</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text2</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text3</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text4</a></li>";
-      ihtml[idx++] = "      </ul>";
-      ihtml[idx++] = "    </li>";
-      ihtml[idx++] = "    <li>";
-      ihtml[idx++] = "      <a href=\"#\">menu3</a>";
-      ihtml[idx++] = "      <ul>";
-      ihtml[idx++] = "        <li><a href=\"#\">text1</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text2</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text3</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text4</a></li>";
-      ihtml[idx++] = "      </ul>";
-      ihtml[idx++] = "    </li>";
-      ihtml[idx++] = "    <li>";
-      ihtml[idx++] = "      <a href=\"#\">menu4</a>";
-      ihtml[idx++] = "      <ul>";
-      ihtml[idx++] = "        <li><a href=\"#\">text1</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text2</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text3</a></li>";
-      ihtml[idx++] = "        <li><a href=\"#\">text4</a></li>";
-      ihtml[idx++] = "      </ul>";
-      ihtml[idx++] = "    </li>";
-      ihtml[idx++] = "  </ul>";
-      ihtml[idx++] = "</aside>";
-      
-  
-      return ihtml.join("");
-    }
+  constructor(div, name) {
+    this._div = div;
+
+    this._name = name;
+
+    VanillaSideBar.I = this;
+
+    this.init();
   }
-  
+
+  init() {
+    let innerHTML = this._createHTML();
+    this._appendHTML(innerHTML);
+  }
+
+  _appendHTML(innerHTML) {
+    this._div.innerHTML = innerHTML;
+  }
+
+  _createHTML() {
+    let name = this._name;
+    let ihtml = [];
+    let idx = 0;
+
+    ihtml[idx++] = "<aside class='side-bar primiary-color'>";
+    ihtml[idx++] = "  <section class='side-bar__icon-box'>";
+    ihtml[idx++] = "    <section class='side-bar__icon-1'>";
+    ihtml[idx++] = "      <div></div>";
+    ihtml[idx++] = "      <div></div>";
+    ihtml[idx++] = "      <div></div>";
+    ihtml[idx++] = "    </section>";
+    ihtml[idx++] = "  </section>";
+    ihtml[idx++] = "  <ul>";
+
+    ihtml[idx++] =
+      "    <li><a href='#' class='item'><div>프로필</div></a></li>";
+    ihtml[idx++] =
+      "    <li><a href='#' class='item'><div>데이터사용량</div></a></li>";
+    ihtml[idx++] = "    <li><a href='#' class='item'><div>내URL</div></a></li>";
+    ihtml[idx++] =
+      "    <li><a href='#' class='item'><div>구매내역</div></a></li>";
+    ihtml[idx++] =
+      "    <li><a href='#' class='item'><div>추천목록</div></a></li>";
+    ihtml[idx++] = "    <li><a href='#' class='item'><div>설정</div></a></li>";
+
+    ihtml[idx++] = "  </ul>";
+    ihtml[idx++] = "</aside>";
+
+    return ihtml.join("");
+  }
+}
